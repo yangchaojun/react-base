fe71d17fed8c7b23fe3421a475d8f5ec4cacc25c
杨超俊 <yangchaojunxs@126.com>
2020-01-17 23:27:32 +0800

    React基础

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import TemperatureCalculator from './TemperatureCalculator'
 
 function App() {
+
   return (
     <div className="App">
+      <TemperatureCalculator />
     </div>
   );
 }
