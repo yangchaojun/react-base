0e8bd29156db991314f267bdb266d65166cbc236
yangchaojun <yangchaojunxs@126.com>
2020-02-12 16:24:11 +0800

    测试jsonholder静态服务

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 const Home = () => {
+    useEffect(() => {
+        fetch("https://my-json-server.typicode.com/yangchaojun/json-static-service/users")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+    }) 
+
     return (
         <div>Home 页面</div>
     )
