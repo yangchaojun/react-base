1f3b6daeb2fe6d9d66803736a4b25a2b6bff8314
yangchaojun <PRD-Chaojun.Yang@Winwayworld.com>
2020-03-14 15:15:11 +0800

    删除无用文件

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import TemperatureCalculator from './TemperatureCalculator'
 
 function App() {
 
   return (
     <div className="App">
-      <TemperatureCalculator />
+      Hello world
     </div>
   );
 }
